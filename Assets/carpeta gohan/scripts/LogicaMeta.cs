cc4cb185b2c904c536f61877649827da631f9b68
G0H4N5gh <gohanharold@gmail.com>
2026-08-21 15:11:57 -0500

    menu de prueba listo y script de la puerta ampliado

--- Assets/carpeta gohan/scripts/LogicaMeta.cs
+++ Assets/carpeta gohan/scripts/LogicaMeta.cs
@@ -6,11 +6,14 @@ public class LogicaMeta : MonoBehaviour
 {
     [Header("Monedas Totales Necesarias")]
   [SerializeField] int monedasNecesarias;
+  [SerializeField] int monedasTotales;
   [Header("Nombre de la siguiente escena")]
-  [SerializeField] string nombreSiguienteEscena;
+  [SerializeField] string nombreSiguienteEscena, nombreEscenaExtra;
   [Header("Tag del Jugador")]
   [SerializeField] string tagJugador;
-  
+  [Header("Booleano para el evento extra")]
+  [SerializeField] bool eventoExtra;
+
   BoxCollider collider;
 
     void Start()
@@ -25,23 +28,39 @@ public class LogicaMeta : MonoBehaviour
     void Update()
     {
         DesbloquearMeta();
+        DesbloquearMetaEventoExtra();
     }
     // Método para desbloquear la meta si se han recolectado suficientes monedas
     void DesbloquearMeta()
     {
         if(GameManager.instance.monedas >= monedasNecesarias)
         {
+            eventoExtra = false;
             collider.enabled = true;
         }
+      
+
         
 
+    }
+    void DesbloquearMetaEventoExtra()
+    {
+        if(GameManager.instance.monedas == monedasTotales)
+        {
+            eventoExtra = true;
+        }
+       
     }
     // Método que se ejecuta cuando el jugador entra en contacto con la meta
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag(tagJugador))
+        if(other.CompareTag(tagJugador) && !eventoExtra)
         {
             SceneManager.LoadScene(nombreSiguienteEscena);
+        }
+        else if(other.CompareTag(tagJugador) && eventoExtra)
+        {
+            SceneManager.LoadScene(nombreEscenaExtra);
         }
     }
 }
